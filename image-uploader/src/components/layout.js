e868dde0621c42d220511e6a5a5e78f5d27cdaad
Mark <mark.jones@ectelect.co.za>
2021-01-17 16:53:34 +0200

    Name changes

---
+++ image-uploader/src/components/layout.js
@@ -0,0 +1,56 @@
+/**
+ * Layout component that queries for data
+ * with Gatsby's StaticQuery component
+ *
+ * See: https://www.gatsbyjs.org/docs/static-query/
+ */
+
+import React from 'react'
+import PropTypes from 'prop-types'
+import { StaticQuery, graphql } from 'gatsby'
+
+import Header from './header'
+import './layout.css'
+
+const Layout = ({ children }) => {
+  return (
+    <StaticQuery
+      query={graphql`
+        query SiteTitleQuery {
+          site {
+            siteMetadata {
+              title
+            }
+          }
+        }
+      `}
+      render={data => (
+        <div style={{ minHeight: '100vh', backgroundColor: '#FFF' }}>
+          {/* <Header siteTitle={data.site.siteMetadata.title} /> */}
+          <Header siteTitle="Image Uploader" />
+          <div
+            style={{
+              margin: `0 auto`,
+              maxWidth: 960,
+              padding: `0px 1.0875rem 1.45rem`,
+              paddingTop: 100,
+            }}
+          >
+            <main>{children}</main>
+            <footer style={{ paddingTop: 10 }}>
+              Built with
+              {` `}
+              <a href="https://www.gatsbyjs.org">Gatsby</a>
+            </footer>
+          </div>
+        </div>
+      )}
+    />
+  )
+}
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
