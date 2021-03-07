17024e45f23eaee157c8df9c30e5495c5bf80c54
Mark <mark.jones@ectelect.co.za>
2021-03-07 15:49:09 +0200

    imports

--- image-uploader/src/pages/landing.js
+++ image-uploader/src/pages/landing.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
-import ImageDisplay from '../components/ImageDisplay';
+import ImageDisplay from '../components/imageDisplay';
 
 const Landing = () => (
   <Layout>
